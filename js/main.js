38d3df602285ba038f063229a073652bffe10bd4
WilliamZheng21 <zheng95z@gmail.com>
2016-02-18 05:24:47 -0500

    movement

--- js/main.js
+++ js/main.js
@@ -4,6 +4,11 @@ window.onload = function() {
     
     var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
+    var cursors;
+    var player;
+    var left;
+    var right;
+    
     function preload() {
         game.load.image( 'floor', 'assets/Floor.png' );
         game.load.spritesheet( 'player', 'assets/player_sprite.png', 50, 50, 8);
@@ -13,12 +18,51 @@ window.onload = function() {
         // Floor background
         game.add.sprite(0, 0, 'floor');
         
-        var player = game.add.sprite(300, 200, 'player');
-        var walk = player.animations.add('walk');
+        player = game.add.sprite(300, 200, 'player', 3);
+        player.smoothed = false;
         
-        player.animations.play('walk', 1, true);
+        left = player.animations.add('left', [1,2], 60, true);
+        right = player.animations.add('right', [7,8], 60, true);
+        player.animations.add('up', [5,6], 60, true);
+        player.animations.add('down', [3,4], 60, true);
+        
+        left.enableUpdate = true;
+        right.enableUpdate = true;
+        
+        game.physics.enable(player, Phaser.Physics.ARCADE);
+        
+        game.camera.follow(player);
+        
+        cursors = game.input.keyboard.createCursorKeys();
     }
     
     function update() {
+        player.body.velocity.set(0);
+        
+        if (cursors.left.isDown)
+        {
+            player.body.velocity.x = -50;
+            player.play('left');
+        }
+        else if (cursors.right.isDown)
+            {
+                player.body.velocity.x = 50;
+                player.play('right');
+            }
+        else if (cursors.up.isDown)
+            {
+                player.body.velocity.y = -50;
+                player.play('up');
+            }
+        else if (cursors.down.isDown)
+            {
+                player.body.velocity.y = 50;
+                player.play('down');
+            }
+        else
+            {
+                player.animations.stop();
+            }
     }
+    
 };
