9335c2f24698e3c562ba1b0016d055204b04aee2
WilliamZheng21 <zheng95z@gmail.com>
2016-02-18 14:55:44 -0500

    last change

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ window.onload = function() {
     var wallBot;
     var wallLeft;
     var wallRight;
+    var door;
     
     var oneblock;
     var twoblockside;
@@ -17,6 +18,7 @@ window.onload = function() {
     var fiveblockside;
     var sixblock;
     var eightblock;
+    var eightblocktwo;
     
     var cursors;
     var player;
@@ -32,15 +34,18 @@ window.onload = function() {
     var current;
     var loopCount = 0;
     
+    var end;
+    
     function preload() 
     {
+        game.load.spritesheet('player', 'assets/player_sprite.png', 50, 50, 8);
+        game.load.spritesheet('bat', 'assets/bat_sprite.png', 45, 36, 4);
         game.load.image('floor', 'assets/Floor.png' );
         game.load.image('wallTop', 'assets/wall_bound_top.png');
         game.load.image('wallBot', 'assets/wall_bound_bot.png');
         game.load.image('wallLeft', 'assets/wall_bound_left.png');
         game.load.image('wallRight', 'assets/wall_bound_right.png');
-        game.load.spritesheet('player', 'assets/player_sprite.png', 50, 50, 8);
-        game.load.spritesheet('bat', 'assets/bat_sprite.png', 45, 36, 4);
+        game.load.image('door', 'assets/opening.png');
         game.load.image('1block', 'assets/1block.png');
         game.load.image('2blockside', 'assets/2blockside.png');
         game.load.image('2blockvert', 'assets/2blockvert.png');
@@ -50,6 +55,7 @@ window.onload = function() {
         game.load.image('6block', 'assets/6block.png');
         game.load.image('8block', 'assets/8block.png');
         game.load.audio('bgMusic', 'assets/Abyss.mp3');
+        game.load.image('end', 'assets/end.png');
 
     }
     
@@ -91,15 +97,47 @@ window.onload = function() {
         wallRight.body.checkCollision.up = false;
         wallRight.body.checkCollision.down = false;
 
+        // Opening
+        door = game.add.sprite(368, 30, 'door');
+        game.physics.enable(door, Phaser.Physics.ARCADE);
+        door.body.immovable = true;
+        
         // Maze setup
-        //oneblock = game.add.sprite(197, 120, '1block');
-        //oneblock = game.add.sprite(405, 125, '1block');
-        //oneblock = game.add.sprite(405, 175, '1block'); 
-        //game.physics.enable(oneblock, Phaser.Physics.ARCADE);
-        //oneblock.body.immovable = true;
-        //oneblock.body.checkCollision.up = true;
+        oneblock = game.add.sprite(199, 124, '1block');
+        oneblock = game.add.sprite(405, 125, '1block');
+        oneblock = game.add.sprite(558, 175, '1block'); 
+        oneblock = game.add.sprite(405, 228, '1block');
+        oneblock = game.add.sprite(199, 380, '1block');
+        oneblock = game.add.sprite(252, 483, '1block');
+        oneblock = game.add.sprite(713, 380, '1block');
+        oneblock = game.add.sprite(98, 277, '1block');
+        twoblockside = game.add.sprite(47, 124, '2blockside');
+        twoblockside = game.add.sprite(508, 480, '2blockside');
+        twoblockside = game.add.sprite(459, 175, '2blockside');
+        threeblockvert = game.add.sprite(304, 75, '3blockvert');
+        eightblock = game.add.sprite(306, 430, '8block');
+        fiveblockside = game.add.sprite(154,227, '5blockside');
+        eightblocktwo = game.add.sprite(510, 280, '8block');
+        twoblockvert = game.add.sprite(510, 74, '2blockvert');
+        
+        game.physics.enable(oneblock, Phaser.Physics.ARCADE);
+        game.physics.enable(twoblockside, Phaser.Physics.ARCADE);
+        game.physics.enable(threeblockvert, Phaser.Physics.ARCADE);
+        game.physics.enable(fiveblockside, Phaser.Physics.ARCADE);
+        game.physics.enable(eightblock, Phaser.Physics.ARCADE);
+        game.physics.enable(eightblocktwo, Phaser.Physics.ARCADE);
+        game.physics.enable(twoblockvert, Phaser.Physics.ARCADE);
+        
+        oneblock.body.immovable = true;
+        twoblockside.body.immovable = true;
+        threeblockvert.body.immovable = true;
+        fiveblockside.body.immovable = true;
+        eightblock.body.immovable = true;
+        eightblocktwo.body.immovable = true;
+        twoblockvert.body.immovable = true;
+        
         // Player
-        player = game.add.sprite(300, 200, 'player', 2);
+        player = game.add.sprite(710, 530, 'player', 2);
         player.smoothed = false;
         
         left = player.animations.add('left', [0,1], 8, true);
@@ -128,6 +166,16 @@ window.onload = function() {
         game.physics.arcade.collide(player, wallBot);
         game.physics.arcade.collide(player, wallLeft);
         game.physics.arcade.collide(player, wallRight);
+        game.physics.arcade.collide(player, oneblock);
+        game.physics.arcade.collide(player, twoblockside);
+        game.physics.arcade.collide(player, eightblock);
+        game.physics.arcade.collide(player, eightblocktwo);
+        game.physics.arcade.collide(player, fiveblockside);
+        game.physics.arcade.collide(player, threeblockvert);
+        game.physics.arcade.collide(player, twoblockvert);
+        
+        // Escape
+        game.physics.arcade.collide(player, door, collisionHandler, null, this);
         
         player.body.velocity.set(0);
         
@@ -158,7 +206,7 @@ window.onload = function() {
             }
 
         // Bat summons
-        if (totalBats < 10)
+        if (totalBats < 5)
             {
                 summonBat();
             }
@@ -192,6 +240,12 @@ window.onload = function() {
         game.physics.arcade.moveToObject(bat, player, 20);
         
         totalBats++;
+    }
+    
+    function collisionHandler (obj1, obj2)
+    {
+        game.world.removeAll();
+        var end = game.load.sprite(266, 264, 'end');
     }
    
 };
