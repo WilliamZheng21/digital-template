ae43d68501cb5de3e7f1f3addcf1ed1c57d1c4b3
WilliamZheng21 <zheng95z@gmail.com>
2016-02-18 04:43:47 -0500

    player sprite

--- js/main.js
+++ js/main.js
@@ -5,16 +5,18 @@ window.onload = function() {
     var game = new Phaser.Game(800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image('Logo', 'assets/phaser.png')
         game.load.image( 'floor', 'assets/Floor.png' );
+        game.load.spritesheet( 'player', 'assets/player_sprite.png', 50, 50, 8);
     }
-    
-    var bouncy;
     
     function create() {
         // Floor background
         game.add.sprite(0, 0, 'floor');
+        
+        var player = game.add.sprite(300, 200, 'player');
+        var walk = player.animations.add('walk');
+        
+        player.animations.play('walk', 60, true);
     }
     
     function update() {
