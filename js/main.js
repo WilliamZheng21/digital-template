2b09d31a6cea6ebfd5cb8f6a7dfd124142953740
WilliamZheng21 <zheng95z@gmail.com>
2016-02-18 03:42:22 -0500

    Floor

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ window.onload = function() {
     
     function preload() {
         // Load an image and call it 'logo'.
+        game.load.image('Logo', 'assets/phaser.png')
         game.load.image( 'floor', 'assets/Floor.png' );
     }
     
@@ -13,7 +14,7 @@ window.onload = function() {
     
     function create() {
         // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
+        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'floor' );
         // Anchor the sprite at its center, as opposed to its top-left corner.
         // so it will be truly centered.
         bouncy.anchor.setTo( 0.5, 0.5 );
